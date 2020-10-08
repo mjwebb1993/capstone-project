f27dded41e28e52be841eae815ef3d986ba0bb73
Michael Webb <webby12333@yahoo.com>
2020-10-08 13:02:35 -0500

    Working Chat

--- src/main/java/com/example/capstoneproject/config/WebSocketConfig.java
+++ src/main/java/com/example/capstoneproject/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").withSockJS();
+        registry.addEndpoint("/capstone").setAllowedOrigins("*").withSockJS();
     }
 
     @Override
